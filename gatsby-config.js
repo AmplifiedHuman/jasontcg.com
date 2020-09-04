ab3144af7a88762b9d334ecdd962ee8d00c2816c
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-09-04 18:39:18 +0800

    Removed unnecessary files

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ const fullConfig = resolveConfig(tailwindConfig);
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Tailwind`,
-    description: `Gatsby starter styled with Tailwind`,
-    author: `@taylorbryant`,
+    title: `Jason Tee`,
+    description: `A personal website developed using Gatsby`,
+    author: `Jason Tee`,
   },
   plugins: [
     `gatsby-plugin-eslint`,
@@ -15,11 +15,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-tailwind`,
-        short_name: `starter`,
+        name: `personal-website`,
+        short_name: `personal-website`,
         start_url: `/`,
         background_color: fullConfig.theme.colors.white,
-        theme_color: fullConfig.theme.colors.teal["400"],
+        theme_color: fullConfig.theme.colors.teal['400'],
         display: `minimal-ui`,
         icon: `src/images/tailwind-icon.png`,
       },
