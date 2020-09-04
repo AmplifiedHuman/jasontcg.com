0d482dfe7c5b2e56fdbea0badef8c18d057b15c1
AmplifiedHuman <amplifiedhuman@gmail.com>
2020-09-04 23:14:55 +0800

    added basic footer

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import styled from 'styled-components';
+
+const Footer = () => {
+  return (
+    <footer className='bg-teal-600'>
+      <nav className='flex flex-col justify-center max-w-4xl p-4 mx-auto text-md md:p-3 md:flex-row text-black'>
+        <p>© 2020 — Made with ♡ by Jason Tee</p>
+      </nav>
+    </footer>
+  );
+};
+
+export default Footer;
